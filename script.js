87706d63a0bcb4a8a249314cc54fa119e5f33268
Ritik-koliyal <koliyalritik50@gmail.com>
2024-07-14 18:47:41 +0530

    seacond commit add

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $("#contactUsButton").click(function () {
+    $("#contactFormModal").modal("show");
+  });
+
+  // Example: Form submission using jQuery
+  $("#contactForm").submit(function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "https://getform.io/f/byvpkyga",
+      method: "POST",
+      data: $(this).serialize(),
+      success: function (data) {
+        alert("Form submitted successfully!");
+        $("#contactForm")[0].reset();
+        $("#contactFormModal").modal("hide");
+      },
+      error: function (err) {
+        alert("Error submitting form.");
+      },
+    });
+  });
+});
+
+function changeImage(imageSrc) {
+  document.getElementById("project-image").src = `/Images/${imageSrc}`;
+}
